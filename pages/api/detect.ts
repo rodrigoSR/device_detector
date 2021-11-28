007c00e9e8ca0db666f93aa81595b20f87a22cec
Rodrigo Rosa <rodrigo.sr6@gmail.com>
2021-11-28 16:13:38 -0300

    chore: add cors

--- pages/api/detect.ts
+++ pages/api/detect.ts
@@ -3,15 +3,24 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import DeviceDetector from "device-detector-js";
 import { PrismaClient, Device, Prisma } from "@prisma/client";
 import { DeviceService } from "../../services/device.service";
+import Cors from "cors";
+import initMiddleware from "../../lib/init-middleware";
 
 const detector = new DeviceDetector();
 const prisma = new PrismaClient();
 const deviceService = new DeviceService(prisma);
+const cors = initMiddleware(
+  Cors({
+    methods: ["GET", "POST", "OPTIONS"],
+    origin: "*",
+  })
+);
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Device>
 ) {
+  await cors(req, res);
   const device = detector.parse(req.headers["user-agent"] || "");
   const createdDevice = await deviceService.create({
     userAgent: req.headers["user-agent"] || "",
